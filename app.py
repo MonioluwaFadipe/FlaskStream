f181e433bbd1833390d77c0be73937c082dc9d98
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-12-13 01:40:50 +0100

    Create sample data

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
+#create mock database
+fakeDatabase = {
+    1:{'name':'Clean car'},
+    2:{'name':'Write blog'},
+    3:{'name':'Start stream'},
+}
 
 #create class-based views
 class Items(Resource):
     def get(self):
-        return 
+        return fakeDatabase
 
